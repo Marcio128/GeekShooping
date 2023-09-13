6fd0e15836c88b243d3cc66419ab86debb2d2396
Márcio <marciaohbs@gmail.com>
2023-09-12 23:39:52 -0300

    first commit

---
+++ GeekShooping.ProductAPI/Model/Contex/MySQLContex.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShooping.ProductAPI.Model.Contex
+{
+    public class MySQLContex : DbContext
+    {
+        public MySQLContex() {}
+        public MySQLContex(DbContextOptions<MySQLContex> options) : base(options) {}
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ GeekShooping.ProductAPI/Program.cs
@@ -0,0 +1,32 @@
+
+using GeekShooping.ProductAPI.Model.Contex;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var connection = builder.Configuration.GetConnectionString("MySQLConnectionString");
+builder.Services.AddDbContext<MySQLContex>(options => options.UseMySql(connection,
+        MySqlServerVersion.AutoDetect(connection)
+        ));
+Console.WriteLine(connection);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
